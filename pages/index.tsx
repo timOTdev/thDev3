b91e5a6cd5b7e9020aecd9527cc2ab2f4c9af87f
timOTdev <tim@timothyhoang.dev>
2022-02-08 17:48:50 -0600

    Add Blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,21 @@ import Introduction from '../components/Introduction';
 import Portfolio from '../components/Portfolio';
 import About from '../components/About';
 import Skills from '../components/Skills';
+import Blog from '../components/Blog';
 import Contact from '../components/Contact';
 
-export default function Home() {
+export default function Home({ props: any }) {
   return (
     <>
       <Head>
-        <title>Welcome to my portfolio site!</title>
+        <title>Timothy Hoang | Software Engineer</title>
       </Head>
       <Layout>
         <Introduction />
         <Portfolio />
         <About />
         <Skills />
+        <Blog />
         <Contact />
       </Layout>
     </>
